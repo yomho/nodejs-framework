f59d6b355c0b7c567b46e4b31b3e331ded84228d
yomho <1550438388@qq.com>
2018-01-23 17:58:07 +0800

    完善demo

--- static/public/app/props.js
+++ static/public/app/props.js
@@ -8,15 +8,15 @@
                 },
                 css: {
                     require: function () {
-                        require('css!../../resource/styles/jquery-ui/jquery-ui.min.css');
-                        require('css!../../resource/styles/jquery-ui/jquery-ui.structure.min.css');
-                        require('css!../../resource/styles/jquery-ui/jquery-ui.theme.min.css');
+                        // require('css!../../resource/styles/jquery-ui/jquery-ui.min.css');
+                        // require('css!../../resource/styles/jquery-ui/jquery-ui.structure.min.css');
+                        // require('css!../../resource/styles/jquery-ui/jquery-ui.theme.min.css');
                     }
                 },
                 js: {
                     require: function () {
-                        require('jquery');
-                        require('jquery-ui.min');
+                        // require('jquery');
+                        // require('jquery-ui.min');
                     }
                 }
             },
